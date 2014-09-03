f1521909ef8dc3887a8694ed6641e4c63d6db3ab
Steve Shogren <steve.a.shogren@gmail.com>
2014-09-03 11:41:46 -0400

    Starting to work on a mocked client side service

--- app/src/tutorial_client/behavior.clj
+++ app/src/tutorial_client/behavior.clj
@@ -1,5 +1,6 @@
 (ns ^:shared tutorial-client.behavior
     (:require [clojure.string :as string]
+              [io.pedestal.app :as app]
               [io.pedestal.app.messages :as msg]))
 
 (defn set-value-transform [old-value message]
@@ -8,10 +9,20 @@
 (defn inc-transform [old-value _]
   ((fnil inc 0) old-value))
 
+(defn init-main [_]
+  [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
+
+(defn swap-transform [_ message]
+  (:value message))
+
 (def example-app
   {:version 2
    :transform [[:inc [:my-counter] inc-transform]
-               [:set-value [:greeting] set-value-transform]]})
+               [:swap [:**]         swap-transform]
+               [:set-value [:greeting] set-value-transform]]
+
+   :emit [{:init init-main}
+          [#{[:*]} (app/default-emitter [:main])]]})
 
 
 
